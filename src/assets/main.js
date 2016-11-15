fd165f88e06dcbe80b5322c880bdf1fe8c2808bd
Renato <rjsilva@outlook.com>
2016-11-15 16:07:07 -0200

    correção 2

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/1195032.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            $.each(response.courses.completed, function (index, item) {
+                console.log('i ' + index + ' v ' + item);
+                $("#badges").append("<div class='course'><h3>" + item.title + "</h3><img src='" + item.badge + "'/><a href='" + item.url + "' class='btn btn-primary' taget='_blank'>See Course</a></div>")
+            });
+        }
+    });
 
 });
